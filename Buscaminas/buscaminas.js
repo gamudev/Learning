8b173fa0ac07c3e98e0623b07fc45a75b5ee1d96
JGM10 <javier10gm@gmail.com>
2022-10-20 23:38:07 +0200

    nuevas funciones

--- Buscaminas/buscaminas.js
+++ Buscaminas/buscaminas.js
@@ -1,3 +1,5 @@
+let bombas = [];
+
 $("#iniciarBuscaminas").click(function(){
     let tamaño = $("#tamaño").val();
     console.log(tamaño);
@@ -6,23 +8,40 @@ $("#iniciarBuscaminas").click(function(){
             $("#tablero").append("<button id='casilla" + (i * 10 + j) + "' class='tablero'></button>");
         }
     }
-    $("#tablero").css("width",20*tamaño);
+    $("#tablero").css("width",40*tamaño);
     $("#seleccionarTamaño").css("visibility","hidden");
 
-    colocarBombas(tamaño)
+    colocarBombas(tamaño);
+    console.log(bombas);
+    addEvents();
 });
 
 function colocarBombas(tamaño){
     let numBombas = tamaño / 2;
-    let bombas = [];
     while (numBombas != 0){
-        posicion = Math.random(tamaño * tamaño);
+        posicion = Math.floor(Math.random() * (tamaño * tamaño));
         if (!bombas.includes(posicion)){
             numBombas--;
-            bombas.append(posicion);
+            bombas.push(posicion);
         }
     }
 }
 
+function addEvents(){
+    $(".tablero").click(function () {
+        let casilla = $(this).attr("id");
+        calcularCasillasCercanas();
+        if (bombas.includes(Number(casilla.substring(7,9)))){
+            $(this).html("B"); 
+            derrota();
+        } else{
+            $(this).html("-");
+        }
+    });
 
+    function derrota() {
+        $("body").append("<span>Has perdido</span");
+        $(".tablero").unbind("click");
+    }
+}
 
