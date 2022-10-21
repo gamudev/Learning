65ebeb1b829e6082bdf82c6f7c6039df008d10ef
JGM10 <javier10gm@gmail.com>
2022-10-21 23:01:32 +0200

    calcularCasillasProximas, calcularBombasProximas

--- Buscaminas/buscaminas.js
+++ Buscaminas/buscaminas.js
@@ -1,7 +1,10 @@
 let bombas = [];
+let casillasProximas = [];
+var tamaño = 10;
 
 $("#iniciarBuscaminas").click(function(){
-    let tamaño = $("#tamaño").val();
+    tamaño = $("#tamaño").val();
+    casillasProximas = [(-tamaño - 1), -tamaño, (-tamaño + 1), -1, 1, (tamaño - 1), tamaño, (tamaño + 1)];
     console.log(tamaño);
     for(let i = 0 ; i < tamaño ; i++){
         for (let j = 0; j < tamaño; j++) {
@@ -10,13 +13,12 @@ $("#iniciarBuscaminas").click(function(){
     }
     $("#tablero").css("width",40*tamaño);
     $("#seleccionarTamaño").css("visibility","hidden");
-
     colocarBombas(tamaño);
-    console.log(bombas);
+    console.log("Bombas: " + bombas);
     addEvents();
 });
 
-function colocarBombas(tamaño){
+function colocarBombas(){
     let numBombas = Math.floor(tamaño / 2);
     while (numBombas != 0){
         posicion = Math.floor(Math.random() * (tamaño * tamaño));
@@ -29,13 +31,13 @@ function colocarBombas(tamaño){
 
 function addEvents(){
     $(".tablero").click(function () {
-        let casilla = $(this).attr("id");
-        // calcularCasillasCercanas();
-        if (bombas.includes(Number(casilla.substring(7,9)))){
+        let casilla = Number($(this).attr("id").substring(7,9));
+        $(this).css("background-color","gainsboro");
+        if (bombas.includes(casilla)){
             $(this).html("B"); 
             derrota();
         } else{
-            $(this).html("-");
+            calcularCasillasProximas(casilla);
         }
     });
 
@@ -43,5 +45,39 @@ function addEvents(){
         $("body").append("<span>Has perdido</span");
         $(".tablero").unbind("click");
     }
+
+    function calcularCasillasProximas(casilla){
+        casillasProximas.forEach(function(casillaProxima){
+            if (esTablero(casilla)) return;
+            let bombasProximas = calcularBombasProximas(casilla+casillaProxima);
+            $("#casilla" + (casilla + casillaProxima)).css("background-color", "gainsboro").html(bombasProximas);
+        });
+    }
+
+    function calcularBombasProximas(casilla){
+        let bombasProximas = 0;
+        casillasProximas.forEach(function (casillaProxima) {
+            if (esTablero(casilla, casilla+casillaProxima)) return;
+            if (bombas.includes(casilla+casillaProxima)){
+                bombasProximas++;
+            }
+        });
+        return bombasProximas;
+    }
+
+    function esTablero(casilla, casillaProxima){
+        console.log("Primera fila: " + (casilla < tamaño) + " -> casilla " + casilla + " tamaño " + tamaño );
+        console.log("Ultima fila: " + (casilla >= (tamaño * (tamaño - 1)) + " -> casilla " + casilla + " tamaño " + (tamaño * (tamaño - 1)) ));
+        console.log("Primera columna: " + (casilla % tamaño == 0) + " -> casilla " + casilla + " tamaño " + (tamaño * (tamaño - 1)) );
+        console.log("Ultima columna: " + (casilla >= (tamaño * (tamaño - 1)) + " -> casilla " + casilla + " tamaño " + (tamaño * (tamaño - 1)) ));
+        if ((casilla < tamaño && (casillaProxima == (-tamaño - 1) || casillaProxima == (-tamaño) || casillaProxima == (-tamaño + 1)))
+        || (casilla % tamaño == 0 && (casillaProxima == (-tamaño - 1) || casillaProxima == -1 || casillaProxima == (tamaño - 1))) 
+        || (casilla % (tamaño - 1) == 0 && (casillaProxima == (tamaño - 1) || casillaProxima == 1 || casillaProxima == (tamaño + 1))) 
+        || (casilla >= (tamaño * (tamaño - 1)) && (casillaProxima == (tamaño - 1) || casillaProxima == tamaño || casillaProxima == (tamaño + 1)))) 
+            return true;
+        return false;
+    }
+
+
 }
 
