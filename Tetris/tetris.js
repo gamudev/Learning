725ab6fc607d0a2e4d64aaf1315cae227a7a21a5
JGM10 <javier10gm@gmail.com>
2022-11-13 22:32:24 +0100

    Update tetris.js

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -4,94 +4,72 @@ $(document).ready(function () {
     let context = juego.getContext("2d");
     let continuar = true;
 
-    const FORMA = {
-        I:1,
-        O:2,
-        T:3,
-        S:4,
-        Z:5,
-        J:6,
-        L:7
+    const POS = {
+        arribaIzq: { x: 120, y: 5 },
+        arribaCen: { x: 140, y: 5 },
+        arribaDer: { x: 160, y: 5 },
+        medioIzq: { x: 120, y: 25 },
+        medioCen: { x: 140, y: 25 },
+        medioDer: { x: 160, y: 25 },
+        abajoIzq: { x: 120, y: 45 },
+        abajoCen: { x: 140, y: 45 },
+        abajoDer: { x: 160, y: 45 },
     }
 
     iniciarJuego();
 
     function iniciarJuego(){
         nuevaPieza();
+        
     }
 
     function nuevaPieza(){
-        while(continuar){
-            let forma = Math.round(Math.random() * 7);
+        // while(continuar){
+            let figura = Math.round(Math.random() * 6);
             let posicion = 1;
-            pintarForma(forma, posicion);
-            mover(forma, posicion);
-        }
+            pintarFigura(figura, posicion);
+            setTimeout(function () {
+                mover(figura, posicion);
+                console.log("accede");
+            }, 100);
+        // }
     }
 
-    function mover(forma, posicion){
+    function mover(figura, posicion){
         posicion++;
-        pintarForma(forma, posicion);
-        if(colocada()){
-            return;
-        } else {
-            mover(forma, posicion);
-        }
+        pintarFigura(figura, posicion);
+        setTimeout(function () {
+            mover(figura, posicion);
+        }, 1000);
+        // if(colocada()){
+        //     return;
+        // } else {
+        //     mover(figura, posicion);
+        // }
     }
 
     function colocada(){
         return true;
     }
 
-    function pintarForma(forma, posicion){
-        switch(forma){
-            case 1: pintarI(posicion);break;
-            case 1: pintarI(posicion);break;
-            case 2: pintarO(posicion);break;
-            case 3: pintarT(posicion);break;
-            case 4: pintarS(posicion);break;
-            case 5: pintarZ(posicion);break;
-            case 6: pintarJ(posicion);break;
-            case 7: pintarL(posicion);break;
+    function pintarFigura(figura, posicion) {
+        console.log(figura);
+        switch (figura){
+            // figura I
+            case 0: puntos = [POS.abajoCen,POS.medioCen,POS.arribaCen];break;
+            // figura T
+            case 1: puntos = [POS.arribaIzq, POS.arribaCen, POS.arribaDer, POS.medioCen]; break;
+            // figura O
+            case 2: puntos = [POS.arribaCen, POS.arribaDer, POS.medioCen, POS.medioDer]; break;
+            // figura S
+            case 3: puntos = [POS.medioIzq, POS.medioCen, POS.arribaCen, POS.arribaDer]; break;
+            // figura Z
+            case 4: puntos = [POS.arribaIzq, POS.arribaCen, POS.medioCen, POS.medioDer]; break;
+            // figura J
+            case 5: puntos = [POS.arribaCen, POS.medioCen, POS.abajoCen, POS.abajoIzq]; break;
+            // figura L
+            case 6: puntos = [POS.arribaCen, POS.medioCen, POS.abajoCen, POS.abajoDer]; break;
         }
-    }
-
-    function pintarI(posicion){
-        const puntos = [{ x: 140, y: 5 },{ x: 140, y: 25 },{ x: 140, y: 45 }]
-        pintar(puntos, posicion);
-    }
-
-    function pintarT(posicion){
-        const puntos = [{ x: 120, y: 5 }, { x: 140, y: 5 }, { x: 160, y: 5 }, { x: 140, y: 25 }]
-        pintar(puntos, posicion);
-    }
-
-    function pintarO(posicion) {
-        const puntos = [{ x: 120, y: 5 }, { x: 140, y: 5 }, { x: 120, y: 25 }, { x: 140, y: 25 }]
-        pintar(puntos), posicion;
-    }
-    
-    function pintarS(posicion) {
-        const puntos = [{ x: 120, y: 25 }, { x: 120, y: 45 }, { x: 140, y: 5 }, { x: 140, y: 25 }]
-        pintar(puntos, posicion);
-    }
-
-    function pintarZ(posicion){
-        const puntos = [{ x: 120, y: 5 }, { x: 140, y: 5 }, { x: 140, y: 25 }, { x: 160, y: 25 }]
-        pintar(puntos, posicion);
-    }
-
-    function pintarJ(posicion){
-        const puntos = [{x:140,y:5},{x:140,y:25},{x:140,y:45},{x:120,y:45}]
-        pintar(puntos, posicion);
-    }
-    
-    function pintarL() {
-        const puntos = [{ x: 140, y: 5 }, { x: 140, y: 25 }, { x: 140, y: 45 }, { x: 160, y: 45 }]
-        pintar(puntos);
-    }
-
-    function pintar(puntos, posicion){
         context.beginPath();
         context.fillStyle = "#000000";
         for (let punto of puntos) {
